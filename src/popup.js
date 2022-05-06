5c5da299d960ee70733786cc8e9014a58584d4de
1cadumagalhaes <carlos.magalhaes@dp6.com.br>
2022-05-06 14:49:35 -0300

    feat(TSHEETS): colocando app para clockar/desclockar

--- src/popup.js
+++ src/popup.js
@@ -5,32 +5,55 @@ function clickHandler(e) {
         code: "receiveInput(" + horas + ")",
         allFrames: true
     });
-	
-	// Save it using the Chrome extension storage API.
-    chrome.storage.sync.set({'horas': horas}, function() {
-          // Notify that we saved.
-		console.log('Settings saved');
+
+    // Save it using the Chrome extension storage API.
+    chrome.storage.sync.set({ 'horas': horas }, function () {
+        // Notify that we saved.
+        console.log('Settings saved');
     });
 }
 
+function openTab(event, tab) {
+    let tabcontent = document.getElementsByClassName("tabcontent")
+    let tablinks = document.getElementsByClassName("tablinks")
+
+
+    for (let content of tabcontent) {
+        content.style.display = "none"
+    }
+
+    for (let link of tablinks) {
+        link.classList.remove("active")
+    }
+    document.getElementById(tab).style.display = "block"
+    event.currentTarget.classList.add("active")
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('updateBtn').addEventListener('click', clickHandler);
-    
 
-    document.getElementById('toggleEdit').addEventListener('click', function(){
-    document.getElementById('timeinput').classList.toggle('active');
 
-    chrome.storage.sync.get('horas', function(items) {
-        console.log("LOADED: " + items['horas']);
-        document.getElementById('hoursInput').value = items['horas'];
-        document.getElementById('hoursOutput').value = items['horas'];
-        clickHandler();
+    document.getElementById('toggleEdit').addEventListener('click', function () {
+        document.getElementById('timeinput').classList.toggle('active');
+
+        chrome.storage.sync.get('horas', function (items) {
+            console.log("LOADED: " + items['horas']);
+            document.getElementById('hoursInput').value = items['horas'];
+            document.getElementById('hoursOutput').value = items['horas'];
+            clickHandler();
+        });
+
     });
-    
-});
 
-    
+    let tabs = document.getElementsByClassName("tablinks")
+    for (let i = 0; i < tabs.length; i++) {
+        let el = tabs[i]
+        el.addEventListener('click', function (e) {
+            openTab(e, `tab${i + 1}`)
+        })
+    }
+
+
 });
 
